9963f13c730158f04a58fb6ed19f8d4f262ca517
Sung J Byun <sungjbyun@D-10-19-156-188.dhcp4.washington.edu>
2018-06-06 19:01:38 -0700

    updated with state

--- client/main.jsx
+++ client/main.jsx
@@ -1,5 +1,3 @@
-'use strict';
-
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,29 +1,94 @@
-'use strict';
-
 import React, { Component } from 'react';
+// from material-ui
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
-import Player from './Player';
+import { List } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+// other components to use
 import TeamList from './Team-list';
 import TeamStats from './Team-stats';
-
+import Player from './Player';
+// for styling
+import { H2 } from '../styled/App.style';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    // set up state
+    this.state = { playsers: [] };
+  }
+  // if components mount, load new state
+  componentWillMount() {
+    this.setState({ players: [
+      {
+        _id: 1,
+        name: 'COLTON FORD',
+        balls_stretchiness: 'N/A',
+        explosiveness_at_climax: 'N/A',
+        topping_aptitude: 'N/A',
+        versatile_capabilities: 'N/A',
+        appreciating_dOrifice: 'N/A',
+        kissing: 'N/A',
+        general_game_strategy: 'N/A',
+        coaxing_for_more: 'N/A',
+        image_url: 'COLTON_FORD.jpg'
+      },
+      {
+        _id: 2,
+        name: 'NICCOLO NERI',
+        balls_stretchiness: 'N/A',
+        explosiveness_at_climax: 'N/A',
+        topping_aptitude: 'N/A',
+        versatile_capabilities: 'N/A',
+        appreciating_dOrifice: 'N/A',
+        kissing: 'N/A',
+        general_game_strategy: 'N/A',
+        coaxing_for_more: 'N/A',
+        image_url: 'NICCOLO_NERI.jpg'
+      },
+      {
+        _id: 3,
+        name: 'KRIS EVANS',
+        balls_stretchiness: 3,
+        explosiveness_at_climax: 3,
+        topping_aptitude: 3,
+        versatile_capabilities: 3,
+        appreciating_dOrifice: 3,
+        kissing: 2,
+        general_game_strategy: 3,
+        coaxing_for_more: 3,
+        image_url: 'KRIS_EVANS.jpg'
+      }
+    ]});
+  }
+
+  renderPlayers() {
+    return this.state.players.map(player => (
+      <TeamList key={player._id} player={player} />
+    ));
+  }
+
   render() {
     return (
       <MuiThemeProvider>
         <div className='container'>
           <AppBar
-            style={{ color: 'red' }}
-            title='Soccer App'
+            title='Model App'
             iconClassNameRight='muidocs-icon-navigation-expand-more'
             showMenuIconButton={false}
           />
-          <div className='row'>
-          <div className='col s12 m7'><Player /></div>
-          <div className='col s12 m5'><TeamList /></div>
-          <div className='col s12 m5'><TeamStats /></div>
+          <div>
+            <div className='col s12 m7'><Player /></div>
+            <div className='col s12 m5'>
+              <H2>Team List</H2>
+              <Divider />
+                <List>
+                  {this.renderPlayers()}
+                </List>
+              <Divider />
+            </div>
+            <div className='col s12 m5'><TeamStats /></div>
           </div>
         </div>
       </MuiThemeProvider>
